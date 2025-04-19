f32a88d1af056469af2af84010400e7c3c531664
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-04-19 17:37:46 +0530

    feat: optionally create cache if it doesn't exist

--- src/stages/configure.ts
+++ src/stages/configure.ts
@@ -10,9 +10,21 @@ export const configure = async () => {
 		const cache = core.getInput("cache");
 		const token = core.getInput("token");
 		const skipUse = core.getInput("skip-use");
+		const create = core.getInput("create");
 
 		core.info("Logging in to Attic cache");
 		await exec("attic", ["login", "--set-default", cache, endpoint, token]);
+
+		if (create === "true") {
+			core.info("Creating Attic cache");
+			try {
+				await exec("attic", ["cache", "create", "--public", cache]);
+			} catch (err: any) {
+				core.info("Attic cache already exists, continuing...");
+			}
+		} else {
+			core.info("Skip Attic cache creation");
+		}
 
 		if (skipUse === "true") {
 			core.info("Not adding Attic cache to substituters as skip-use is set to true");
